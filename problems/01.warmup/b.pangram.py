55b9dc1056d65e57b296742cb697a302827f6e43
Terry Chern <terry@yoolen.dev>
2025-08-03 22:23:20 -0400

    clean up styling

---
+++ problems/01.warmup/b.pangram.py
@@ -0,0 +1,10 @@
+# 2. Pangram
+class Solution:
+    def checkIfPangram(self, sentence):
+        """
+        After removing all whitespace and non-alphabetical characters,
+        check if the count of unique characters (ignoring case) is
+        exactly the same as the number of characters in the English
+        alphabet.
+        """
+        return len(set(''.join([char.lower() for char in sentence if char.isalpha()]))) == 26
