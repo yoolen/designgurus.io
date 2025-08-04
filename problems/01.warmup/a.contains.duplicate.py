52c7205767f46379c4c6d556ba499752ddd15811
Terry Chern <terry@yoolen.dev>
2025-08-03 22:22:58 -0400

    clean up styling

---
+++ problems/01.warmup/a.contains.duplicate.py
@@ -0,0 +1,10 @@
+# 1. Contains Duplicate
+class Solution:
+    def containsDuplicate(self, nums):
+        """
+        If the count of unique characters (whether 
+        found through set or a dict) is shorter than
+        the length of the list, then there must be 
+        duplicate elements.
+        """
+        return len(set(nums)) != len(nums)
