7cff37415318e35fe98aab13972e7359e251190d
Terry Chern <terry@yoolen.dev>
2025-08-04 19:41:47 -0400

    add shortest distance between words

---
+++ problems/01.warmup/f.shortest.word.distance.py
@@ -0,0 +1,21 @@
+class Solution:
+    def shortestDistance(self, words, word1, word2):
+        """
+        Since all words must be in sequence, we can traverse the word
+        list once and track when we encountered each word most recently.
+        Then at each alternating encounter, calculate the distance from
+        the last encountered "other" word since any previous encounters
+        must be further.
+        """
+        curr_1 = curr_2 = None
+        shortest = len(words)
+        for idx, word in enumerate(words):
+            if word == word1:
+                curr_1 = idx
+                if curr_2 is not None:
+                    shortest = min(shortest, idx - curr_2)
+            if word == word2:
+                curr_2 = idx
+                if curr_1 is not None:
+                    shortest = min(shortest, idx - curr_1)
+        return shortest
