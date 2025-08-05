93538a711057d4a031abd74b4b0647ccc1f7598b
Terry Chern <terry@yoolen.dev>
2025-08-04 20:03:50 -0400

    add number of good pairs

---
+++ problems/01.warmup/g.number.of.good.pairs.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+from math import comb
+
+
+class Solution:
+    def numGoodPairs(self, nums):
+        """
+        Find the indices for each number since a number has to be the same
+        to have a pair. We can then use the formula for the number of valid
+        combinations nCr (we don't care about the ordering outside of knowing
+        that there is one valid order where the first number is less than the
+        second) to calculate the number of good pairs.
+        """
+        pairs = defaultdict(list)
+        for idx, num in enumerate(nums):
+            pairs[num].append(idx)
+        return sum(comb(len(idxs), 2) for idxs in pairs.values())
