8a47d76120e08b537ff309dfaed93064e0cf56af
Terry Chern <terry@yoolen.dev>
2025-08-04 23:00:33 -0400

    add square root

---
+++ problems/01.warmup/h.square.root.py
@@ -0,0 +1,28 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        """
+        For any number x, its square root will lie between 0 and x/2.
+        Use binary search to find the closest integer rounded down.
+        Find the bounds and then split the search space in half
+        each time to see which half the square root might be in.
+        """
+        if x < 2:
+            return x  # All x <= 1 are their own square root
+        # For all other values iterate over the bounds until we arrive at a solution
+        # or cross the bounds, at which point we know that the right bound was the
+        # answer (since we are rounding down immediately).
+        left, right = 0, x // 2
+        # Bisect the data in half each time by searching the midpoint and then
+        # assigning a new left or right. Since we're checking the midpoint we
+        # already know that it can't be the answer and should offset by 1 more.
+        while left <= right:
+            midpoint = (left + right) // 2
+            square = midpoint**2
+            # Check which half of the remaining values contains x
+            if x > square:
+                left = midpoint + 1
+            elif x < square:
+                right = midpoint - 1
+            else:
+                return midpoint
+        return right
